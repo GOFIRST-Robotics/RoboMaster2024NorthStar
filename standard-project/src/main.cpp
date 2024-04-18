6c2fb58a234dce5f30c99849d3292182d1360aeb
IsaacPatton <23pattisa@isd748.org>
2024-04-18 03:25:54 -0500

    ideas

--- standard-project/src/main.cpp
+++ standard-project/src/main.cpp
@@ -75,9 +75,9 @@ int main()
     Board::initialize();
     initializeIo(drivers);
     robot.initSubsystemCommands();
-    // drivers->pwm.setTimerFrequency(tap::gpio::Pwm::Timer::TIMER1, 200);
-    // drivers->pwm.write(0.5, tap::gpio::Pwm::Pin::C1);
-    // drivers->pwm.write(0.5, tap::gpio::Pwm::Pin::C2);
+    // drivers->pwm.setTimerFrequency(tap::gpio::Pwm::Timer::TIMER1, 250);
+    // drivers->pwm.write(0.4, tap::gpio::Pwm::Pin::C1);
+    // drivers->pwm.write(0.4, tap::gpio::Pwm::Pin::C2);
     
 
 #ifdef PLATFORM_HOSTED
